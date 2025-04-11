0dac43ccd29b6586abd63d656cce6b79a5781c8b
laggykiller <chaudominic2@gmail.com>
2025-04-11 11:53:50 +0800

    1.3.7

--- src/rlottie_python/__init__.py
+++ src/rlottie_python/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """rlottie-python"""
 
-__version__ = "1.3.6"
+__version__ = "1.3.7"
 
 from .rlottie_wrapper import LottieAnimation  # type: ignore # noqa: F401
 from .rlottie_wrapper import LottieAnimationProperty  # type: ignore # noqa: F401
